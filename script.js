bec9e6f28b36c3b74dc58f3622964277c70b2bb2
SegiToxic <61798558+SegiToxic@users.noreply.github.com>
2024-04-03 18:22:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function openNav() {
-  const nav = document.getElementById("navigation");
-  const socials = document.getElementById("socials");
+const nav = document.getElementById("navigation");
+const socials = document.getElementById("socials");
 
-  nav.style.display = "flex";
-  socials.style.display = "flex";
+function openNav() {
+  if (window.innerWidth < 700) socials.style.display = "flex";
   document.documentElement.style.overflowY = "hidden";
+  nav.style.display = "flex";
+  nav.style.animation = "fade-in 1s normal";
+  nav.style.width = "40vw";
 }
 function closeNav() {
-  const nav = document.getElementById("navigation");
-  const socials = document.getElementById("socials");
-
-  nav.style.display = "none";
   socials.style.display = "none";
   document.documentElement.style.overflowY = "auto";
+  nav.style.animation = "fade-out 1s normal";
+  nav.style.width = "0";
+  setTimeout(function () {
+    nav.style.display = "none";
+  }, 1000);
 }
+
+function checkScreenWidth() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth >= 1024) {
+    nav.style.display = "flex";
+    socials.style.display = "none";
+  } else {
+    nav.style.display = "none";
+    socials.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", checkScreenWidth);
+
+websiteLoad();
