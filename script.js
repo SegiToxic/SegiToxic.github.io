f5059f9a918c96dc8f5d6d83b4306efc08701202
SegiToxic <61798558+SegiToxic@users.noreply.github.com>
2024-04-04 16:55:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,36 @@ const socials = document.getElementById("socials");
 
 function openNav() {
   if (window.innerWidth < 700) socials.style.display = "flex";
-  document.documentElement.style.overflowY = "hidden";
+  if (window.innerWidth >= 700 && window.innerWidth < 1023) {
+    nav.style.width = "40vw";
+    nav.style.animation = "fade-in 1s normal";
+  } else {
+    nav.style.width = "100%";
+  }
   nav.style.display = "flex";
-  nav.style.animation = "fade-in 1s normal";
-  nav.style.width = "40vw";
+  document.documentElement.style.overflowY = "hidden";
 }
 function closeNav() {
   socials.style.display = "none";
-  document.documentElement.style.overflowY = "auto";
-  nav.style.animation = "fade-out 1s normal";
-  nav.style.width = "0";
-  setTimeout(function () {
+  if (window.innerWidth >= 700 && window.innerWidth < 1023) {
+    document.documentElement.style.overflowY = "auto";
+    nav.style.animation = "fade-out 1s normal";
+    nav.style.width = "0";
+    setTimeout(function () {
+      nav.style.display = "none";
+    }, 1000);
+  } else {
     nav.style.display = "none";
-  }, 1000);
+  }
+  document.documentElement.style.overflowY = "auto";
 }
 
 function checkScreenWidth() {
   const screenWidth = window.innerWidth;
-
+  if (screenWidth >= 1024 || screenWidth < 700) {
+    nav.style.width = "100%";
+    nav.style.animation = "0";
+  }
   if (screenWidth >= 1024) {
     nav.style.display = "flex";
     socials.style.display = "none";
